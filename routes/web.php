64832798f4896f74c5abf4aa377babbffca9ce7f
joaovsfonseca <joao_victorsf@outlook.com.br>
2026-02-27 10:55:01 -0300

    feat

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Serve Quasar SPA for all routes (except /api and static files)
-Route::get('/{any?}', function ($any = null) {
-    // If a path is provided and the file exists, serve it directly
-    if ($any) {
-        $filePath = public_path($any);
-
-        if (file_exists($filePath) && is_file($filePath)) {
-            return response()->file($filePath);
-        }
-    }
-
-    // Otherwise, serve the SPA
-    $indexPath = public_path('index.html');
-
-    if (!file_exists($indexPath)) {
-        return response()->json([
-            'message' => 'Frontend não compilado. Execute "npm run build" na pasta resources/app'
-        ], 503);
-    }
-
-    return file_get_contents($indexPath);
-})->where('any', '.*');
+// Rota raiz - página padrão do Laravel
+Route::get('/', function () {
+    return view('welcome');
+});
